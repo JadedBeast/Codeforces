6179d821580bedfc6493b92d5aa7785a448fef2e
Bahae-Eddine JMAHRI <41241372+JadedBeast@users.noreply.github.com>
2020-02-23 13:22:31 +0100

    Create B.cpp

---
+++ 630/B.cpp
@@ -0,0 +1,39 @@
+//       In the name of         ****                      God                     ****
+ 
+/*
+ 
+Copyright (C)          JadedBeast
+ 
+                                   Morocco
+ 
+created : 05/11/2019
+ 
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define oo 1e17
+#define ll long long 
+#define debug(x) cout<<"Mayday Mayday : "<<(x)<<endl;
+inline void JadedBeast() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+const int MAX=2e3+5;
+
+
+int main() {
+    JadedBeast();
+   	double n,t;
+   	cout.precision(20);
+    double mor=1.000000011;
+    cin >>n >> t;
+
+    cout<< n*pow(mor,t);
+
+    
+
+	return 0;
+}		
+
