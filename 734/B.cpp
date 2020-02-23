2fd7e5a67d3838350f4f3052d28c46a94cba7083
Bahae-Eddine JMAHRI <41241372+JadedBeast@users.noreply.github.com>
2020-02-23 13:31:00 +0100

    Create B.cpp

---
+++ 734/B.cpp
@@ -0,0 +1,29 @@
+//       In the name of         ****                      God                     ****
+
+/*
+
+Copyright (C)          JadedBeast
+
+                                   Morocco
+
+created : 5/3/2019
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+inline void Boost() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+}
+
+int main(void){
+    Boost();
+    int k2,k3,k5,k6;
+    cin >> k2 >>k3 >>k5 >>k6;
+
+    cout << 256*min(k2,min(k5,k6))+32*min(k2-min(k2,min(k5,k6)),k3)<<endl;
+    
+   return 0;
+}
