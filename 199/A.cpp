3bdc33d7394b4482b1209f488fddf0908f9f1e12
Bahae-Eddine JMAHRI <41241372+JadedBeast@users.noreply.github.com>
2020-02-23 01:01:08 +0100

    Create A.cpp

---
+++ 199/A.cpp
@@ -0,0 +1,31 @@
+//       In the name of         ****                      God                     ****
+
+/*
+
+Copyright (C)          JadedBeast
+
+                                   Morocco
+
+created : 24/3/2019
+	
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline void JadedBeast() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+}
+
+int fib[100];
+set <int> s;
+
+int main(void) {
+	JadedBeast();
+    int n;
+	cin >> n;
+	cout << n << " " << 0 <<" " << 0 <<endl;
+	
+}
