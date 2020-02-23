7e0251dca738c8339689eaed4aa29803b85ac0bb
Bahae-Eddine JMAHRI <41241372+JadedBeast@users.noreply.github.com>
2020-02-23 13:36:22 +0100

    Create A.c

---
+++ 805/A.c
@@ -0,0 +1,20 @@
+//       In the name of         ˜”*°•.˜”*°•                      God                     •°*”˜.•°*”˜
+ 
+/*  
+ 
+            JadedBeast                                             ~~Morocco~~
+ 
+*/
+#include <stdio.h>
+ 
+int main(void){
+    long long l,r;
+    scanf("%I64d %I64d",&l,&r);
+    if(l<r){
+        printf("2");
+    }else{
+        printf("%I64d",l);
+    }
+   
+    return 0;
+}
