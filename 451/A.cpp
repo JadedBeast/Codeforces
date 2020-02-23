64daa67a0825fe36124a2e94bf42fc5a09ce9eab
Bahae-Eddine JMAHRI <41241372+JadedBeast@users.noreply.github.com>
2020-02-23 01:25:15 +0100

    Create A.cpp

---
+++ 451/A.cpp
@@ -0,0 +1,30 @@
+//       In the name of         ****                      God                     ****
+
+/*
+
+Copyright (C)          JadedBeast
+
+                                    Morocco
+
+created : 21/2/2019
+
+*/
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main(void)
+{
+    int n,m;
+    cin>>n >> m;
+
+    if(min(n,m)%2==0){
+        cout<<"Malvika"<<endl;
+    }else{
+        cout<< "Akshat"<<endl;
+    }
+
+    return 0;
+}
