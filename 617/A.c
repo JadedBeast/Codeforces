95d57edf464bacdea27e7871e461e71b4e293b68
Bahae-Eddine JMAHRI <41241372+JadedBeast@users.noreply.github.com>
2020-02-23 13:21:19 +0100

    Create A.c

---
+++ 617/A.c
@@ -0,0 +1,18 @@
+//       In the name of         ˜”*°•.˜”*°•                      God                     •°*”˜.•°*”˜
+ 
+/*  
+ 
+            JadedBeast                                             ~~Morocco~~
+ 
+*/
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    int x;
+    scanf("%d",&x);
+    printf("%d",(int)ceil((float)x/5));
+   
+   
+    return 0;
+}
